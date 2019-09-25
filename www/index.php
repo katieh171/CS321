6bc60220456d791189ff92d12423a0cea577f823
katieh171 <mccarver@simmons.edu>
2019-09-25 12:57:43 -0400

    This is for Assignment 2

--- www/index.php
+++ www/index.php
@@ -1,1 +1,33 @@
-This is a test
+<!DOCTYPE html>
+<html>
+<!--html and css portion of website project, created by Katie McCarver for CS321. Used w3schools as reference-->
+<head>
+  <link href="assignment2.css" rel="stylesheet" type="text/css"/>
+  <title>Violas</title>
+
+</head>
+
+<body>
+  <div class ="header">
+      <a class="active" href="#home">Home</a>
+      <a href="#about" >About</a>
+    <a href="#music" >Music</a>
+    <a href="#shopping">Shopping</a>
+    <a href="#sources">Sources</a>
+  </div>
+      
+  <h1>Voila, <em>Violas!</em></h1>
+
+ <!-- <img src "https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&ved=2ahUKEwi5xuG54-fkAhXkguAKHT0GCWYQjRx6BAgBEAQ&url=https%3A%2F%2Fwww.publicdomainpictures.net%2Fen%2Fview-image.php%3Fimage%3D138226%26picture%3Dplaying-the-viola&psig=AOvVaw1GQiH1zj-ZjiCoTd9Rg-2o&ust=1569356700904726" class="center" alt="Person Playing Viola"/>-->
+ 
+  <div id="home">
+    <h2>Home</h2>
+    <p>  Welcome to the webpage all about violas! Here you’ll find a description, brief history, and information about the stringed instrument. There will also be links to some prominent music pieces featuring violas, and links to reputable shops that sell violas and/or accessories. Also included are some of the sources for the information on violas. Enjoy!</p>
+  
+
+<div class="footer">
+  <p>VIOLAS</p>
+    </div>
+
+ </body> 
+</html>
